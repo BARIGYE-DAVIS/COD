c46eb4fd74185c673a9c0f9b8dad2112cd47cabf
BARIGYE-DAVIS <barigyedavis6@gmail.com>
2024-04-17 14:51:05 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //echo "Hello World!"
+
+        $name = "John";
+        echo "MY name is john $name";
+    ?>
+   <?php
+    $code = "html, css, php, javascript" ;
+ echo "<p>I truely love  $code! because the are interesting webdevelopment laguages and friendly</p>";
+?>
+    <form action="">
+<?php
+    $name = "John";
+    $age = 25;
+ echo  "<p> MY name is  $name and am $age years old</p>"
+?>
+<?php
+ echo "<p>We are private tour operators base in Uganda aka The pearl of Africa </p>";
+?>
+<?php
+?>
+    </form>
+</body>
+</html>
